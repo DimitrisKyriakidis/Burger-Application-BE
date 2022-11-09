19b1bbacd7f1adcc894c1373a298e4986ef74820
DimitrisKyriakidis95 <dimikyri1@hotmail.com>
2022-11-10 01:03:41 +0200

    seeders

--- seeders/orders.js
+++ seeders/orders.js
@@ -11,18 +11,53 @@ module.exports = [{
     {
         id: "arertyr456",
         image: "https://media.istockphoto.com/id/1254672762/photo/delicious-homemade-hamburger-and-french-fries.jpg?b=1&s=170667a&w=0&k=20&c=a0kPiHhzTncW-_LdM7cXcLiB9KLXuEb8KAJt7AlVuO0=",
-        orderPrice: 4,
+        orderPrice: 4.4,
         quantity: 0,
         comment: "",
         status: 100,
     },
-    // {
-    //     id: "gjs34654",
-    //     image: "../assets/images/3.png",
-    //     orderPrice: 4,
-    //     quantity: 0,
-    //     selected: false,
-    //     comment: "",
-    //     status: 100,
-    // },
+    {
+        id: "gjs34654",
+        image: "https://www.tefal.com.au/medias/?context=bWFzdGVyfHJvb3R8MTk1Nzd8aW1hZ2UvanBlZ3xoMzkvaDljLzE2MDY3MDAwMzM2NDE0LmpwZ3w3ZDQ0ZTU5NjlkYjgxMzI1ZmYxNjI1NTIwOWY0MDdjYjRiOWI1N2IxYmI2ZjkyZmYzMGQ5M2U4YTA4ZGQ0Njdk",
+        orderPrice: 4.8,
+        quantity: 0,
+        comment: "",
+        status: 100,
+    },
+
+    {
+        id: "certt6563ad",
+        image: "https://media.istockphoto.com/id/1203580318/photo/vegan-meatless-plant-based-protein-chicken-strip-burger-on-a-whole-wheat-bun-with-lettuce.jpg?s=612x612&w=0&k=20&c=gmwd9ArrKHE_0vX3R0eYLHfAg129rBk2EzmcsSBFxlk=",
+        orderPrice: 5.6,
+        quantity: 0,
+        comment: "",
+        status: 100,
+    },
+
+    {
+        id: "ngdty43th",
+        image: "https://media.istockphoto.com/photos/fried-chicken-burger-picture-id652832752?k=20&m=652832752&s=612x612&w=0&h=D8EdmX-vOI44TnrLGv0JLJXQi2jxg3ye9XF9c3MSmEM=",
+        orderPrice: 3.8,
+        quantity: 0,
+        comment: "",
+        status: 100,
+    },
+
+    {
+        id: "fhdsl34e",
+        image: "https://www.theperfectloaf.com/wp-content/uploads/2017/06/theperfectloaf-brioche-hamburger-buns-9-1080x721.jpg",
+        orderPrice: 4.7,
+        quantity: 0,
+        comment: "",
+        status: 100,
+    },
+
+    {
+        id: "htrytr372",
+        image: "https://img.freepik.com/premium-photo/delicious-double-burger-with-beef-tomatoes-lettuce-melted-cheese-white-parchment-dark-surface-american-fast-food_207126-6342.jpg?w=2000",
+        orderPrice: 4.2,
+        quantity: 0,
+        comment: "",
+        status: 100,
+    },
 ];
